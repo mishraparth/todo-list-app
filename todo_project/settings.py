0caaf1be9844e19658437ee44ff6e3c38a88512f
Parth Mishra <34400268+mishraparth@users.noreply.github.com>
2025-10-22 21:14:43 +0530

    Update settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -87,6 +87,9 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 6,
+        }
     },
 ]
 
