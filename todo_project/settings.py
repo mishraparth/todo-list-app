9f99e3814c29f7d6812385ed0946f37c3f4c46d3
Parth Mishra <34400268+mishraparth@users.noreply.github.com>
2025-10-22 21:50:19 +0530

    Update settings.py

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -109,8 +109,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+#STATIC_URL = 'static/'     temporary basis
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'task_list'
 LOGOUT_REDIRECT_URL = 'login'
